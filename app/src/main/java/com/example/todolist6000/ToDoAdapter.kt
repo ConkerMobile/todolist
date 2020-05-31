4aa30d081a81ebaee1444e0f85108ca1123b165f
qingy2019 <qingy2019@outlook.com>
2020-05-30 23:37:03 -0700

    todo list

---
+++ app/src/main/java/com/example/todolist6000/ToDoAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.todolist6000
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.todo_row.view.*
+
+class ToDoAdapter(val todos: List<String>) : RecyclerView.Adapter<ToDoAdapter.ToDoHolder>() {
+    class ToDoHolder(v:View) : RecyclerView.ViewHolder(v), View.OnClickListener{
+
+        var view : View = v
+        var title : String = ""
+
+        init{
+            v.setOnClickListener(this)
+        }
+
+        fun bindToDo(title:String) {
+            this.title = title
+            view.textView.text = title
+        }
+
+        override fun onClick(v: View?) {
+            val intent = Intent(view.context, CompleteToDoActivity::class.java)
+            intent.putExtra("title", title)
+            startActivity(view.context,intent,null)
+        }
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ToDoHolder {
+        return ToDoHolder(LayoutInflater.from(p0.context).inflate(R.layout.todo_row, p0, false))
+    }
+
+    override fun getItemCount(): Int {
+        return todos.count()
+    }
+
+    override fun onBindViewHolder(p0: ToDoHolder, p1: Int) {
+        val title = todos[p1]
+        p0.bindToDo(title)
+    }
+}
